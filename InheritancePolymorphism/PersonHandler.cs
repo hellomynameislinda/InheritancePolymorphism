360820ff2bd70964b4aaaea5588a7416cd0714c7
Linda H <linda@busungar.com>
2024-03-08 09:21:53 +0100

    Add project files.

---
+++ InheritancePolymorphism/PersonHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePolymorphism
+{
+    internal class PersonHandler
+    {
+        public Person CreatePerson(int age, string fname, string lname, double height, double weight)
+        {
+
+            return new Person();
+        }
+
+        public void SetAge(Person pers, int age)
+        {
+            pers.Age = age;
+        }
+
+    }
+}
---
+++ InheritancePolymorphism/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace InheritancePolymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+
+            p.FName = "Testen";
+
+            Console.ReadLine();
+        }
+    }
+}
